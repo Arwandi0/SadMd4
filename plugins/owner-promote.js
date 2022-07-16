11792f422e0dde8111e44bcba46e435214a37f9b
Arwandi00 <100402870+Arwandi00@users.noreply.github.com>
2022-07-17 06:19:34 +0800

    Update owner-promote.js

--- plugins/owner-promote.js
+++ plugins/owner-promote.js
@@ -1,23 +1,17 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, participants }) => {
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let promoteUser = []
-    for (let user of users)
-        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-            await delay(1 * 1000)
-        }
-    m.reply('Succes')
-
+let handler = async (m, { conn, text, participants }) => {
+if (!text) throw `tag someone`
+let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+await conn.groupParticipantsUpdate(m.chat, [users], 'promote')
+m.reply(`@${users.split("@")[0]} is Now admin`)
 }
-handler.help = ['opromote @tag']
-handler.tags = ['owner']
-handler.command = /^(opromote)$/i
-
+handler.help = ['promote'].map(v => v+'@tag')
+handler.tags = ["group"]
+handler.command = /^(promote)$/i
+handler.botAdmin = true
 handler.owner = true
 handler.group = true
-handler.botAdmin = true
 
 export default handler
+
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
